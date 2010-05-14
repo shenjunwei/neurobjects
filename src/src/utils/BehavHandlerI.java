32750f7f409b64f6f984f07b50135b17faf7feca
napvasconcelos <napvasconcelos@ede21ce0-ec6d-f1c9-52f8-815c3ec13f0c>
2010-05-14 19:09:49 +0000

    inclusão definição de colt no projeto.

--- src/src/utils/BehavHandlerI.java
+++ src/src/utils/BehavHandlerI.java
@@ -1,5 +1,11 @@
 package utils;
 
+import java.util.ArrayList;
+import cern.colt.matrix.DoubleMatrix1D;
+
 public interface BehavHandlerI {
+	
+	public ArrayList<DoubleMatrix1D> 		getIntervals(String label);
+	public double[] 						getBigInterval(ArrayList<String> labels);
 
 }
