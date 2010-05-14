815eadf397f1c4516dda3af4263d4f521bc32aed
edson.anibal <edson.anibal@ede21ce0-ec6d-f1c9-52f8-815c3ec13f0c>
2010-05-14 19:35:32 +0000

    Início da documentação

--- src/src/utils/BehavHandlerI.java
+++ src/src/utils/BehavHandlerI.java
@@ -3,6 +3,11 @@ package utils;
 import java.util.ArrayList;
 import cern.colt.matrix.DoubleMatrix1D;
 
+/** @brief Handle animal behavior informations 
+ * 
+ * 
+ *
+ */
 public interface BehavHandlerI {
 	
 	public ArrayList<DoubleMatrix1D> 		getIntervals(String label);
