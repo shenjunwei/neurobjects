efb08125498a19af35e849cec8b5401cc7b47179
napvasconcelos <napvasconcelos@ede21ce0-ec6d-f1c9-52f8-815c3ec13f0c>
2010-06-07 18:05:45 +0000

    novos

--- src/src/app/BehavHandlerFileApp.java
+++ src/src/app/BehavHandlerFileApp.java
@@ -4,6 +4,8 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 import utils.BehavHandlerFile;
+import utils.CountMatrix;
+import utils.TxtSpkHandler;
 public class BehavHandlerFileApp {
 
 	/**
@@ -12,13 +14,28 @@ public class BehavHandlerFileApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		BehavHandlerFile BHF = new BehavHandlerFile ("/tmp/behave.txt");
+		BehavHandlerFile BHF = new BehavHandlerFile ("/home/nivaldo/tmp/nda/ge5_contacts.txt");
+		//String labels = "ball,brush,food,urchin";
+		String labels = "ball,brush,food,urchin";
+		double bigInterval[]={0,0};
+		String path = "/home/nivaldo/projects/crnets/data/spikes/ge5/01/all";
+		String filter = "s1";
 		
 		
+		
+		if (!BHF.isValid()) {
+			return;
+		}
 		BHF.sort();
 		System.out.println(BHF);
+		bigInterval = BHF.getBigInterval(labels);
+		System.out.println("Big Interval for '"+labels+"': "+Arrays.toString(bigInterval));
+		TxtSpkHandler spikes = new TxtSpkHandler (path, filter, bigInterval[0], bigInterval[1]);
+		CountMatrix   matrix = new CountMatrix (spikes,0.25);
+		//Patterns P = new Patterns ()
+		//System.out.println(matrix);
 		
-		System.out.println("Big Interval for 'A,B': "+Arrays.toString(BHF.getBigInterval("A,B")));
+		
 
 	}
 
