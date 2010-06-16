60be65482ce6292985e56ea14ff29c730d40d757
napvasconcelos <napvasconcelos@ede21ce0-ec6d-f1c9-52f8-815c3ec13f0c>
2010-06-16 18:25:49 +0000

    remoção do método getInstances

--- src/src/app/DataSetBuilderApp.java
+++ src/src/app/DataSetBuilderApp.java
@@ -14,6 +14,7 @@ import org.xml.sax.SAXParseException;
 
 import utils.AnimalSetup;
 import utils.DataSetBuilder;
+import utils.Dataset;
 import weka.core.Instances;
 
 public class DataSetBuilderApp {
@@ -50,9 +51,9 @@ public class DataSetBuilderApp {
 						animalList.add(animal);
 						DataSetBuilder D = new DataSetBuilder(animal);
 						System.out.println(animal);
-						Instances dataset[] = D.getInstances("hp", "ball");
-						System.out.println (dataset[0]);
-						System.out.println (dataset[1]);
+						Dataset data = D.get("hp", "ball");
+						System.out.println (data);
+						
 						System.out.println("\n\n");
 						return;
 					}
