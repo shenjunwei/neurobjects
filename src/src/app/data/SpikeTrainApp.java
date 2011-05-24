99012af8fe3d70f6b860803ff59d6d635926169e
giulianoxt <giulianoxt@ede21ce0-ec6d-f1c9-52f8-815c3ec13f0c>
2011-05-24 17:53:16 +0000

    Improved documentation for the test application SpikeTrainApp.

--- src/src/app/data/SpikeTrainApp.java
+++ src/src/app/data/SpikeTrainApp.java
@@ -2,20 +2,39 @@ package app.data;
 
 import java.io.IOException;
 
-import data.SpikeTrain;
-import data.TxtSpkHandler;
-
+import data.*;
 import cern.colt.matrix.DoubleMatrix1D;
 
 
 /**
- * SpikeTrainApp - App test for the SpikeTrain class.
+ * \brief App test for the SpikeTrain class.
  * 
- * Constructs a table containing spike statistics for the V1_12a and V1_16a neurons and
- * writes it to the standard output.
+ * This is a test application for the SpkHandlerI and SpikeTrain components. It shows how
+ * to use the TxtSpkHandler implementation of SpkHandlerI to open a set of text files
+ * containing spike train data. 
  * 
+ * To open a set of spike trains we first need to provide:
+ *  - Path to a directory of text files: \code String path = "/home/..."; \endcode
+ *  - Filter to identify chosen neurons: \code String filter = "V1"; \endcode
+ *  - Desired interval of spike times: \code double a = 0, b = 5820; \endcode
+ * 
+ * Next, we create a TxtSpkHandler with these parameters. It's the component that
+ * reads the spike train raw data and provides a simple access interface (SpkHandlerI). 
+ * \code SpkHandlerI spikeHandler = new TxtSpkHandler(path, filter, a, b); \endcode
+ * 
+ * To actually use the spike train data, we can loop through every spike train using:
+ * \code for (SpikeTrain spikeTrain : spikeHandler.getAllSpikes()) \endcode \encode
+ * (There are other ways to get spike trains, see SpkHandlerI for details)
+ * 
+ * In this app, we use a SpikeTrain to access the activation times and the inter spike
+ * intervals: \code spikeTrain.getTimes() \endcode and \code spikeTrain.getISI() \endcode.
+ * 
+ * Finally, we print to the standard output a table containing basic statistics for every
+ * spike train, such as: neuron name, time of first and last activations, average time of
+ * activation, etc.
+ *  
  * @author giulianoxt
- *
+ * @date May 19, 2011.
  */
 public class SpikeTrainApp {
 	public static void main(String[] args) throws IOException {
@@ -24,7 +43,7 @@ public class SpikeTrainApp {
 		double a = 0;
 		double b = 5820;
 		
-		TxtSpkHandler spikeHandler = new TxtSpkHandler(path, filter, a, b);
+		SpkHandlerI spikeHandler = new TxtSpkHandler(path, filter, a, b);
 		
 		System.out.println("Neuron | Spike min | Spike max | Spike avg | " +
 				           "ISI min | ISI max | ISI avg");
