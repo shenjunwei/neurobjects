29b3f7c05d425b15f4e3fa59e3642444aab34bb8
giulianoxt <giulianoxt@ede21ce0-ec6d-f1c9-52f8-815c3ec13f0c>
2011-05-26 02:12:33 +0000

    Cria classe Interval, com testes.

---
+++ src/nda/data/Interval.java
@@ -0,0 +1,76 @@
+package nda.data;
+
+/**
+ * Represents a time interval (window). 
+ * 
+ * @author giulianoxt
+ */
+public class Interval {
+    protected double start;
+    protected double end;
+    
+    public final static Interval EMPTY = new Interval(
+            Double.NaN, Double.NaN
+    );
+    public final static Interval INF = new Interval(
+            Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY
+    );
+    
+    public static Interval make(double start, double end) {
+        Interval interval = new Interval(start, end);
+        
+        if (!interval.isValid()) {
+            throw new IllegalArgumentException("Interval must have a valid range [a,b]");
+        }
+        else {
+            return interval;
+        }
+    }
+    
+    protected Interval(double start, double end) {
+        this.start = start;
+        this.end = end;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("[%.3f, %.3f]", start, end);
+    }
+    
+    public double start() {
+        return start;
+    }
+    
+    public double end() {
+        return end;
+    }
+    
+    public double duration() {
+        return end - start;
+    }
+    
+    public boolean contains(double t) {
+        return start <= t && t <= end;
+    }
+    
+    public boolean contains(Interval interval) {
+        assert interval.isValid();
+        return start <= interval.start() && interval.end() <= end;
+    }
+    
+    public boolean isValid() {
+        return start <= end;
+    }
+    
+    public boolean isEmpty() {
+        return !isValid();
+    }
+    
+    public Interval intersection(Interval interval) {
+        assert interval.isValid();
+        
+        double a = Math.max(start, interval.start());
+        double b = Math.min(end, interval.end()); 
+        return new Interval(a, b);
+    }
+}
---
+++ test/nda/data/IntervalTest.java
@@ -0,0 +1,102 @@
+package nda.data;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ * Test the Interval class.
+ * 
+ * @author giulianoxt
+ */
+public class IntervalTest {
+    final Interval norm_i = Interval.make(0, 10);
+    final Interval point_i = Interval.make(5, 5);
+    final Interval neg_i = Interval.make(-5, 15);
+    final Interval out_i = Interval.make(20, 30);
+    final double EPS = 1e-8;
+    
+    /**
+     * Test method for {@link nda.data.Interval#make(double, double)}.
+     */
+    @Test
+    public void testMake() {
+        assertEquals(0, norm_i.start(), EPS);
+        assertEquals(10, norm_i.end(), EPS);
+    }
+
+    /**
+     * Test method for {@link nda.data.Interval#duration()}.
+     */
+    @Test
+    public void testDuration() {
+        assertEquals(10, norm_i.duration(), EPS);
+        assertEquals(0, point_i.duration(), EPS);
+    }
+
+    /**
+     * Test method for {@link nda.data.Interval#contains(double)}.
+     */
+    @Test
+    public void testContainsDouble() {
+        assertTrue(norm_i.contains(5));
+        assertTrue(norm_i.contains(0));
+        assertTrue(norm_i.contains(10));
+        assertFalse(norm_i.contains(-5));
+        assertTrue(point_i.contains(5));
+    }
+
+    /**
+     * Test method for {@link nda.data.Interval#contains(nda.data.Interval)}.
+     */
+    @Test
+    public void testContainsInterval() {
+        assertTrue(norm_i.contains(point_i));
+        assertTrue(norm_i.contains(norm_i));
+        assertTrue(neg_i.contains(norm_i));
+        assertFalse(point_i.contains(norm_i));
+    }
+
+    /**
+     * Test method for {@link nda.data.Interval#isValid()}.
+     */
+    @Test
+    public void testIsValid() {
+        assertTrue(neg_i.isValid());
+        assertTrue(point_i.isValid());
+        assertFalse(norm_i.intersection(out_i).isValid());
+    }
+
+    /**
+     * Test method for {@link nda.data.Interval#intersection(nda.data.Interval)}.
+     */
+    @Test
+    public void testIntersection() {
+        assertTrue(neg_i.intersection(norm_i).isValid());
+        assertEquals(0, neg_i.intersection(norm_i).start(), EPS);
+        assertEquals(10, neg_i.intersection(norm_i).end(), EPS);
+        assertFalse(norm_i.intersection(out_i).isValid());
+        assertEquals(0, norm_i.intersection(point_i).duration(), EPS);
+    }
+    
+    /**
+     * Test method for {@link nda.data.Interval#isEmpty()}.
+     */
+    @Test
+    public void testIsEmpty() {
+        assertFalse(norm_i.isEmpty());
+        assertFalse(neg_i.isEmpty());
+        assertTrue(norm_i.intersection(out_i).isEmpty());
+    }
+    
+    /**
+     * Test pre-defined intervals
+     */
+    @Test
+    public void testPreDefined() {
+        assertTrue(Interval.EMPTY.isEmpty());
+        assertFalse(Interval.EMPTY.isValid());
+        assertTrue(Interval.INF.contains(neg_i));
+        assertFalse(norm_i.contains(Interval.INF));
+    }
+}
