d72df58fb9cafcb0c8a69bfe8265bdd45b190577
giulianoxt <giulianoxt@ede21ce0-ec6d-f1c9-52f8-815c3ec13f0c>
2011-05-26 16:56:38 +0000

    Documentação da ReadTextDataApp.

--- test/app/ReadTextDataApp.java
+++ test/app/ReadTextDataApp.java
@@ -10,50 +10,59 @@ import static nda.util.ArrayUtils.*;
 
 
 /**
- * \brief App test for the SpikeTrain class.
- * 
- * This is a test application for the SpkHandlerI and SpikeTrain components. It shows how
- * to use the TxtSpkHandler implementation of SpkHandlerI to open a set of text files
- * containing spike train data. 
+ * This is a test application for the SpikeHandlerI and SpikeTrain components. It shows
+ * how to use the TextSpikeHandler implementation of SpikeHandlerI to open a set of text
+ * files containing spike train data. 
  * 
  * To open a set of spike trains we first need to provide:
- *  - Path to a directory of text files: \code String path = "/home/..."; \endcode
- *  - Filter to identify chosen neurons: \code String filter = "V1"; \endcode
- *  - Desired interval of spike times: \code double a = 0, b = 5820; \endcode
+ *  - Path to a directory of text files: {@code String path = ...;}
+ *  - Filter to identify chosen neurons: {@code String filter = "V1";}
+ *  - Desired interval of spike times: {@code Interval i = Interval.make(a, b);}
  * 
- * Next, we create a TxtSpkHandler with these parameters. It's the component that
- * reads the spike train raw data and provides a simple access interface (SpkHandlerI). 
- * \code SpkHandlerI spikeHandler = new TxtSpkHandler(path, filter, a, b); \endcode
+ * Next, we create a TextSpikeHandler with these parameters. It's the component that
+ * reads the spike train raw data and provides a simple access interface (SpikeHandlerI). 
+ * {@code SpikeHandlerI spikeHandler = new TextSpikeHandler(path, filter, i);}
  * 
  * To actually use the spike train data, we can loop through every spike train using:
- * \code for (SpikeTrain spikeTrain : spikeHandler.getAllSpikes()) \endcode \encode
- * (There are other ways to get spike trains, see SpkHandlerI for details)
+ * {@code for (SpikeTrain spikeTrain : spikeHandler.getAllSpikes())} (There are other
+ * ways to get spike trains, see SpikeHandlerI for details).
  * 
  * In this app, we use a SpikeTrain to access the activation times and the inter spike
- * intervals: \code spikeTrain.getTimes() \endcode and \code spikeTrain.getISI() \endcode.
+ * intervals: {@code spikeTrain.getTimes()} and {@code spikeTrain.getInterSpikeInterval()}.
  * 
  * Finally, we print to the standard output a table containing basic statistics for every
  * spike train, such as: neuron name, time of first and last activations, average time of
  * activation, etc.
  *  
- * @author giulianoxt
+ * @author Giuliano Vilela.
  * @date May 19, 2011.
  */
 public class ReadTextDataApp {
 	public static void main(String[] args) throws Exception {
+	    // Path to a directory with spike files
 		String path = "setup/spikes";
+		
+		// Only open neurons whose names begin with "V1"
 		String filter = "V1";
+		
+		// For each neuron, only load in memory the spikes that occur between
+		// 0s and 5820s.
 	    Interval interval = Interval.make(0, 5820);
 	    
+	    // Create a SpikeHandlerI to effectively open the files
 	    SpikeHandlerI spikeHandler = new TextSpikeHandler(path, filter, interval);
 		
+	    // Print the table header
 		out.println("Neuron | Spike min | Spike max | Spike avg | " +
 				    "ISI min | ISI max | ISI avg");
 		
+		// For each spike train that matches the given criteria
 		for (SpikeTrain spikeTrain : spikeHandler.getAllSpikeTrains()) {
+		    // Load the spike train time series and inter spike intervals
 			double[] times = spikeTrain.getTimes();
 			double[] isi = spikeTrain.getInterspikeInterval();
 				
+			// Print a row of the table
 			out.printf("%s %11.3f %11.3f %11.3f %9.3f %9.3f %9.3f\n",
 					spikeTrain.getName(), spikeTrain.getFirst(), spikeTrain.getLast(),
 					getAverage(times), getMin(isi), getMax(isi), getAverage(isi)
