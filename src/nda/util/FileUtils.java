45170f19f716637aff347591cccd241f27f15302
giulianoxt <giulianoxt@ede21ce0-ec6d-f1c9-52f8-815c3ec13f0c>
2011-06-01 11:49:16 +0000

    Improving Histogram

--- src/nda/util/FileUtils.java
+++ src/nda/util/FileUtils.java
@@ -1,19 +1,76 @@
 package nda.util;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 
 /**
- * @author giulianoxt
- *
+ * 
+ * @author Giuliano Vilela
  */
 public final class FileUtils {
     public static String parseFileName(String filepath) {
         int start = filepath.lastIndexOf('.');
         int end = filepath.lastIndexOf(File.separatorChar);
-        
+
         if (start < 0) start = filepath.length();
 
         String newName = filepath.substring(end+1, start);
         return newName;
+    }
+
+
+    public static int[] readIntArray(String filepath)
+    throws FileNotFoundException, IOException {
+        BufferedReader in = new BufferedReader(new FileReader(filepath));
+        ArrayList<Integer> l = new ArrayList<Integer>();
+        String line;
+
+        while ((line = in.readLine()) != null) {
+            if (line.isEmpty()) continue;
+            l.add(Integer.parseInt(line));
+        }
+
+        int[] res = new int[l.size()];
+        for (int i = 0; i < l.size(); ++i)
+            res[i] = l.get(i);
+
+        return res;
+    }
+
+
+    public static double[] readDoubles(String filepath)
+    throws FileNotFoundException, IOException {
+        BufferedReader in = new BufferedReader(new FileReader(filepath));
+        ArrayList<Double> l = new ArrayList<Double>();
+        String line;
+
+        while ((line = in.readLine()) != null) {
+            if (line.isEmpty()) continue;
+            l.add(Double.parseDouble(line));
+        }
+
+        double[] res = new double[l.size()];
+        for (int i = 0; i < l.size(); ++i)
+            res[i] = l.get(i);
+
+        return res;
+    }
+
+
+    public static void saveIntArray(String filepath, int[] array)
+    throws IOException {
+        BufferedWriter out = new BufferedWriter(new FileWriter(filepath));
+
+        for (int x : array)
+            out.write(Integer.toString(x) + '\n');
+
+        out.close();
     }
 }
