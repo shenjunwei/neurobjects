5f41c1f12771149b257cfcbd3c5464e42d1fd031
giulianoxt <giulianoxt@ede21ce0-ec6d-f1c9-52f8-815c3ec13f0c>
2011-06-01 23:52:37 +0000

    Componente SpikeRateMatrix e sua implementação principal, CountMatrix, junto com testes. TODO: app, docs.

---
+++ src/nda/data/CountMatrix.java
@@ -0,0 +1,284 @@
+package nda.data;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+
+/**
+ * @brief Create the Matrix of spike count given a interval
+ * 
+ * Considering that the spike times are stored in data files, with a specific structure
+ * (one spike time per row), this class build a matrix where the correspondent spike
+ * counting is stored for that given interval.
+ * 
+ * @author Nivaldo Vasconcelos
+ * @author Giuliano Vilela
+ */
+public class CountMatrix implements SpikeRateMatrixI {
+    private Histogram histogram;
+    private int[][] matrix;
+
+    private int cursor_pos;
+    private int cursor_width;
+
+    private String title;
+    private List<String> neuronNames;
+
+
+    public CountMatrix(SpikeHandlerI spikeHandler, int binCount) {
+        Interval interval = spikeHandler.getGlobalSpikeInterval();
+        histogram = new Histogram(interval, binCount);
+
+        load(spikeHandler);
+    }
+
+
+    public CountMatrix(SpikeHandlerI spikeHandler, double binSize) {
+        Interval interval = spikeHandler.getGlobalSpikeInterval();
+        histogram = new Histogram(interval, binSize);
+
+        load(spikeHandler);
+    }
+
+
+    protected void load(SpikeHandlerI spikeHandler) {
+        int n_rows = spikeHandler.getNumberOfSpikeTrains();
+        int n_cols = histogram.getNumberBins();
+        matrix = new int[n_rows][n_cols];
+
+        for (int r = 0; r < n_rows; ++r) {
+            histogram.load(spikeHandler.getSpikeTrain(r));
+            histogram.saveBinCounts(matrix[r]);
+        }
+
+        cursor_pos = 0;
+        cursor_width = 1;
+
+        title = "CountMatrix";
+        neuronNames = spikeHandler.getNeuronNames();
+    }
+
+
+    @Override
+    public int numRows() {
+        return matrix.length;
+    }
+
+
+    @Override
+    public int numColumns() {
+        return histogram.getNumberBins();
+    }
+
+
+    @Override
+    public double getBinSize() {
+        return histogram.getBinSize();
+    }
+
+
+    public int get(int row, int column) {
+        return matrix[row][column];
+    }
+
+
+    public int[] getRow(int row) {
+        return matrix[row];
+    }
+
+
+    public int[] getColumn(int column) {
+        int[] col = new int[numRows()];
+
+        for (int i = 0; i < numRows(); ++i)
+            col[i] = matrix[i][column];
+
+        return col;
+    }
+
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+
+    @Override
+    public List<String> getNeuronNames() {
+        return neuronNames;
+    }
+
+
+    @Override
+    public String toString() {
+        return title +
+        " cursor:" + cursor_pos +
+        " width:" + cursor_width +
+        " matrix:" + Arrays.toString(matrix);
+    }
+
+
+    @Override
+    public double[] getPattern(Interval interval) {
+        if (!containsWindow(interval)) {
+            throw new IllegalArgumentException(
+            "interval lies outside the spike activity boundary");
+        }
+
+        int startBin = histogram.getBinFor(interval.start());
+        int endBin = histogram.getBinFor(interval.end());
+        return getRawPattern(startBin, endBin);
+    }
+
+
+    @Override
+    public double[] getPattern(int width) {
+        if (!containsWindow(cursor_pos, width)) {
+            throw new IllegalArgumentException(
+            "there is no pattern starting from cursor with the desired width");
+        }
+
+        int startBin = cursor_pos++;
+        return getRawPattern(startBin, startBin+width-1);
+    }
+
+
+    @Override
+    public double[] getPattern(double startTime, int width) {
+        if (!containsWindow(startTime, width)) {
+            throw new IllegalArgumentException(
+            "there is no pattern starting from startTime with the desired width");
+        }
+
+        int startBin = histogram.getBinFor(startTime);
+        return getRawPattern(startBin, startBin+width-1);
+    }
+
+
+    @Override
+    public double[] getPattern(int column, int width) {
+        if (!containsWindow(column, width)) {
+            throw new IllegalArgumentException(
+            "there is no pattern starting from column with the desired width");
+        }
+
+        return getRawPattern(column, column+width-1);
+    }
+
+
+    protected double[] getRawPattern(int startBin, int endBin) {
+        int length = endBin - startBin + 1;
+        int pattern_sz = length * numRows();
+        double[] pattern = new double[pattern_sz];
+
+        for (int r = 0, p = 0; r < numRows(); ++r)
+            for (int c = startBin; c <= endBin; ++c)
+                pattern[p++] = matrix[r][c];
+
+        return pattern;
+    }
+
+
+    @Override
+    public Iterator<double[]> iterator() {
+        return new PatternIterator(this);
+    }
+
+
+    @Override
+    public Interval getInterval() {
+        return histogram.getInterval();
+    }
+
+
+    @Override
+    public void setWindowWidth(int width) {
+        if (width >= numColumns())
+            throw new IllegalArgumentException("width exceeds the matrix dimensions");
+
+        cursor_width = width;
+    }
+
+
+    @Override
+    public int getWindowWidth() {
+        return cursor_width;
+    }
+
+
+    @Override
+    public int getCurrentColumn() {
+        if (cursor_pos >= numColumns())
+            return -1;
+        else
+            return cursor_pos;
+    }
+
+
+    @Override
+    public void setTitle(String _title) {
+        title = _title;
+    }
+
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+
+    @Override
+    public int numPatterns(int width) {
+        return Math.max(0, numColumns()-cursor_pos-width);
+    }
+
+
+    @Override
+    public int numPatterns(double startTime, int width) {
+        if (!getInterval().contains(startTime))
+            return 0;
+
+        int p = histogram.getBinFor(startTime);
+        return Math.max(0, numColumns()-p-width);
+    }
+
+
+    @Override
+    public boolean setCurrentTime(double startTime) {
+        try {
+            cursor_pos = histogram.getBinFor(startTime);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+
+    @Override
+    public boolean setCurrentColumn(int column) {
+        if (0 <= column && column < numColumns()) {
+            cursor_pos = column;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+    @Override
+    public boolean containsWindow(Interval interval) {
+        return getInterval().contains(interval);
+    }
+
+
+    @Override
+    public boolean containsWindow(double startTime, double width) {
+        return containsWindow(Interval.make(startTime, startTime + width));
+    }
+
+
+    protected boolean containsWindow(int column, int width) {
+        return column + width <= numColumns();
+    }
+}
---
+++ test/nda/data/CountMatrixTest.java
@@ -0,0 +1,235 @@
+package nda.data;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import nda.data.text.TextSpikeHandler;
+
+
+/**
+ * Tests for the CountMatrix class.
+ * 
+ * @author Giuliano Vilela
+ * @ingroup UnitTests
+ */
+public class CountMatrixTest {
+    private static final double EPS = 1e-10;
+    private static final String spikeDir = "setup/spikes/";
+    private static final String testDir = "setup/test_spikes/";
+
+    private static SpikeHandlerI handler_v1;
+    private static SpikeHandlerI handler_test;
+
+    private CountMatrix cm_v1;
+    private CountMatrix cm_test;
+
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        handler_v1 = new TextSpikeHandler(spikeDir, "V1");
+        handler_test = new TextSpikeHandler(testDir, "test");
+    }
+
+
+    @Before
+    public void setUp() {
+        cm_v1 = new CountMatrix(handler_v1, 0.250);
+        cm_test = new CountMatrix(handler_test, 2.0);
+    }
+
+
+    /**
+     * Test method for {@link nda.data.CountMatrix#CountMatrix(nda.data.SpikeHandlerI, int)}.
+     */
+    @Test
+    public void testCountMatrixSpikeHandlerIInt() {
+        CountMatrix count_m = new CountMatrix(handler_v1, 10);
+
+        assertEquals(10, count_m.numColumns());
+        assertEquals(handler_v1.getNumberOfSpikeTrains(), count_m.numRows());
+        assertTrue(count_m.getInterval().contains(handler_v1.getGlobalSpikeInterval()));
+        assertEquals(handler_v1.getNeuronNames(), count_m.getNeuronNames());
+
+        double exp_bs = handler_v1.getGlobalSpikeInterval().duration() / 10;
+        assertEquals(exp_bs, count_m.getBinSize(), EPS);
+    }
+
+
+    /**
+     * Test method for {@link nda.data.CountMatrix#CountMatrix(nda.data.SpikeHandlerI, double)}.
+     */
+    @Test
+    public void testCountMatrixSpikeHandlerIDouble() {
+        CountMatrix count_m = new CountMatrix(handler_test, 0.5);
+
+        assertEquals(0.5, count_m.getBinSize(), EPS);
+        assertEquals(handler_test.getNumberOfSpikeTrains(), count_m.numRows());
+        assertTrue(count_m.getInterval().contains(handler_test.getGlobalSpikeInterval()));
+        assertEquals(handler_test.getNeuronNames(), count_m.getNeuronNames());
+
+        int exp_bc = (int) (handler_test.getGlobalSpikeInterval().duration() / 0.5);
+        assertEquals(exp_bc, count_m.numColumns());
+    }
+
+
+    /**
+     * Test method for {@link nda.data.CountMatrix#get(int, int)}.
+     */
+    @Test
+    public void testGet() {
+        assertEquals(2, cm_test.get(0, 1));
+        assertEquals(2, cm_test.get(0, 4));
+        assertEquals(0, cm_test.get(0, 5));
+        assertEquals(2, cm_test.get(1, 0));
+        assertEquals(0, cm_test.get(2, 0));
+    }
+
+
+    /**
+     * Test method for {@link nda.data.CountMatrix#getColumn(int)}.
+     */
+    @Test
+    public void testGetColumn() {
+        int exp_sum = 0;
+        for (SpikeTrain st : handler_v1.getAllSpikeTrains())
+            exp_sum += st.getNumberOfSpikes();
+
+        int sum = 0;
+        for (int c = 0; c < cm_v1.numColumns(); ++c)
+            for (int value : cm_v1.getColumn(c))
+                sum += value;
+
+        assertEquals(exp_sum, sum);
+    }
+
+
+    /**
+     * Test method for {@link nda.data.CountMatrix#getMatrix()}.
+     */
+    @Test
+    public void testGetMatrix() {
+        for (int r = 0; r < cm_v1.numRows(); ++r) {
+            int sum = 0;
+            for (int count : cm_v1.getRow(r)) sum += count;
+            assertEquals(handler_v1.getSpikeTrain(r).getNumberOfSpikes(), sum);
+        }
+    }
+
+
+    /**
+     * Test method for {@link nda.data.CountMatrix#getNeuronNames()}.
+     */
+    @Test
+    public void testGetNeuronNames() {
+        assertEquals(handler_v1.getNeuronNames(), cm_v1.getNeuronNames());
+        assertEquals(handler_test.getNeuronNames(), cm_test.getNeuronNames());
+    }
+
+
+    /**
+     * Test method for {@link nda.data.CountMatrix#getPattern(nda.data.Interval)}.
+     */
+    @Test
+    public void testGetPatternInterval() {
+        double[] pattern = cm_test.getPattern(Interval.make(1, 10));
+        checkPattern(pattern, cm_test, 0, 4);
+    }
+
+
+    /**
+     * Test method for {@link nda.data.CountMatrix#getPattern(int)}.
+     */
+    @Test
+    public void testGetPatternInt() {
+        double[] p1 = cm_test.getPattern(5);
+        checkPattern(p1, cm_test, 0, 4);
+
+        double[] p2 = cm_test.getPattern(25);
+        checkPattern(p2, cm_test, 1, 25);
+    }
+
+
+    /**
+     * Test method for {@link nda.data.CountMatrix#getPattern(double, int)}.
+     */
+    @Test
+    public void testGetPatternDoubleInt() {
+        double[] p1 = cm_test.getPattern(2.5, 2);
+        checkPattern(p1, cm_test, 1, 2);
+
+        double first = handler_v1.getGlobalSpikeInterval().start();
+
+        double[] p2 = cm_v1.getPattern(first + cm_v1.getBinSize(), 5);
+        checkPattern(p2, cm_v1, 1, 5);
+    }
+
+
+    /**
+     * Test method for {@link nda.data.CountMatrix#getPattern(int, int)}.
+     */
+    @Test
+    public void testGetPatternIntInt() {
+        checkPattern(cm_test.getPattern(3, 5), cm_test, 3, 7);
+        checkPattern(cm_test.getPattern(8, 2), cm_test, 8, 9);
+        checkPattern(cm_test.getPattern(0, 3), cm_test, 0, 2);
+        checkPattern(cm_test.getPattern(5, 2), cm_test, 5, 6);
+        checkPattern(cm_v1.getPattern(2, 40), cm_v1, 2, 41);
+        checkPattern(cm_v1.getPattern(37, 58), cm_v1, 37, 94);
+    }
+
+
+    /**
+     * Test method for {@link nda.data.CountMatrix#next()}.
+     */
+    @Test
+    public void testForeach() {
+        cm_v1.setWindowWidth(17);
+
+        int c = 0;
+        for (double[] pattern : cm_v1) {
+            checkPattern(pattern, cm_v1, c, c+16);
+            c += 1;
+        }
+
+        cm_v1.setCurrentColumn(0);
+        assertEquals(cm_v1.numPatterns(17), c);
+    }
+
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testRemove() {
+        cm_v1.getPattern(40);
+        cm_v1.getPattern(213);
+        cm_v1.getPattern(12);
+        cm_v1.iterator().remove();
+    }
+
+
+    /**
+     * Test method for {@link nda.data.CountMatrix#containsWindow(nda.data.Interval)}.
+     */
+    @Test
+    public void testContainsWindowInterval() {
+        assertTrue(cm_test.containsWindow(Interval.make(5, 80)));
+        assertFalse(cm_test.containsWindow(Interval.make(1, 101)));
+    }
+
+
+    private void checkPattern(double[] pattern, CountMatrix m, int start_c, int end_c) {
+        int exp_l = m.numRows() * (end_c-start_c+1);
+        assertEquals(exp_l, pattern.length);
+
+        for (int r = 0; r < m.numRows(); ++r) {
+            int row_l = end_c - start_c + 1;
+            for (int c = start_c; c <= end_c; ++c) {
+                int p_i = r * row_l + c - start_c;
+                assertEquals(m.get(r, c), pattern[p_i], EPS);
+            }
+        }
+    }
+}
