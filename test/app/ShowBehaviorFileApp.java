4bf8ea88f8c7f6a6bddcb058a45292bdf49a0f0a
giulianoxt <giulianoxt@ede21ce0-ec6d-f1c9-52f8-815c3ec13f0c>
2011-06-14 17:20:16 +0000

    BehaviorHandler component: implementation, tests and wiki.

---
+++ test/app/ShowBehaviorFileApp.java
@@ -0,0 +1,34 @@
+package app;
+
+import java.util.Set;
+
+import nda.data.BehaviorHandlerI;
+import nda.data.text.TextBehaviorHandler;
+
+
+/**
+ * This is a test application for the BehaviorHandlerI component and its
+ * TextBehaviorHandler implementation. It shows how to read a text data file
+ * containing an animal behavior specification and print it to the user.
+ * 
+ * @author Giuliano Vilela
+ * @ingroup ExampleApps
+ */
+public class ShowBehaviorFileApp {
+    public static void main(String[] args) throws Exception {
+        // Path to the behavior data file
+        String filepath = "setup/behaviors/ge4_contacts.txt";
+
+        // Load the data file into memory using a BehaviorHandlerI
+        BehaviorHandlerI handler = new TextBehaviorHandler(filepath);
+
+        // Access all the labels that were used in the file
+        Set<String> labels = handler.getLabelSet();
+
+        // Print the labels and its intervals
+        for (String label : labels) {
+            System.out.printf("Label \"%s\" is defined for intervals: %s\n",
+                    label, handler.getIntervals(label));
+        }
+    }
+}
