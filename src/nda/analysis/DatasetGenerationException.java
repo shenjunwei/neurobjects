6ab182f5ce66d709a3bfd232612a7e499d59f721
giulianoxt <giulianoxt@ede21ce0-ec6d-f1c9-52f8-815c3ec13f0c>
2011-07-21 03:19:17 +0000

    Add the DatasetGenerator component (and its TextDatasetGenerator implementation).
    Current TODO list: complete DatasetGeneratorTest and make a small app that calls TextDatasetGenerator.generate on some setup file.

---
+++ src/nda/analysis/DatasetGenerationException.java
@@ -0,0 +1,25 @@
+package nda.analysis;
+
+/**
+ * Thrown to indicate that an exceptional error ocurred when
+ * generating datasets from a DatasetGenerator.
+ * 
+ * @author Giuliano Vilela
+ */
+public class DatasetGenerationException extends Exception {
+    private static final long serialVersionUID = -3481591819591142338L;
+
+    public DatasetGenerationException() { }
+
+    public DatasetGenerationException(String message) {
+        super(message);
+    }
+
+    public DatasetGenerationException(Throwable cause) {
+        super(cause);
+    }
+
+    public DatasetGenerationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
