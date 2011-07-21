fc5ccda2718fd0dc0318f5a4295d6da678fb1876
giulianoxt <giulianoxt@ede21ce0-ec6d-f1c9-52f8-815c3ec13f0c>
2011-07-21 15:33:41 +0000

    Tests for the TextDatasetGenerator class.

---
+++ test/nda/analysis/text/TextDatasetGeneratorTest.java
@@ -0,0 +1,61 @@
+package nda.analysis.text;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import nda.analysis.Setup;
+
+
+/**
+ * Tests for the TextDatasetGenerator class.
+ *
+ * @author Giuliano Vilela
+ * @ingroup UnitTests
+ */
+public class TextDatasetGeneratorTest {
+    private File outputDir;
+    private TextDatasetGenerator generator;
+    private static String setupFilepath = "data/test/test_setup_gen.yml";
+
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        Setup setup = new Setup(setupFilepath);
+        String outputDir = setup.getOutputDirectory();
+        File dirFile = new File(outputDir);
+
+        if (!dirFile.exists() && !dirFile.mkdir())
+            throw new IllegalArgumentException("Cant create dir");
+
+        for (File file : dirFile.listFiles())
+            if (file.isFile()) file.delete();
+
+        assertEquals(0, dirFile.listFiles().length);
+    }
+
+
+    @Before
+    public void setUp() throws Exception {
+        Setup setup = new Setup(setupFilepath);
+        outputDir = new File(setup.getOutputDirectory());
+
+        generator = new TextDatasetGenerator(setup);
+        generator.setVerbose(true);
+    }
+
+
+    /**
+     * Test method for {@link nda.analysis.text.TextDatasetGenerator#generate()}.
+     */
+    @Test
+    public void testGenerate() throws Exception {
+        generator.generate();
+        assertTrue(outputDir.listFiles().length > 0);
+    }
+}
