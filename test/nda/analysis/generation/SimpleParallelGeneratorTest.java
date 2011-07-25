1bdf7e31e37152f0ea4a895b6d0daefa7865f4d9
giulianoxt <giulianoxt@ede21ce0-ec6d-f1c9-52f8-815c3ec13f0c>
2011-07-25 18:59:07 +0000

    Tests for the ParallelGenerator classes

---
+++ test/nda/analysis/generation/SimpleParallelGeneratorTest.java
@@ -0,0 +1,61 @@
+package nda.analysis.generation;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import nda.analysis.Setup;
+
+
+/**
+ * Tests for the SimpleParallelGenerator class.
+ *
+ * @author Giuliano Vilela
+ * @ingroup UnitTests
+ */
+public class SimpleParallelGeneratorTest {
+    private File outputDir;
+    private SimpleParallelGenerator generator;
+    private static String setupFilepath = "data/test/test_setup_gen.yml";
+
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        Setup setup = new Setup(setupFilepath);
+        String outputDir = setup.getOutputDirectory();
+        File dirFile = new File(outputDir);
+
+        if (!dirFile.exists() && !dirFile.mkdir())
+            throw new IllegalArgumentException("Cant create dir: " + dirFile);
+
+        for (File file : dirFile.listFiles())
+            if (file.isFile()) file.delete();
+
+        assertEquals(0, dirFile.listFiles().length);
+    }
+
+
+    @Before
+    public void setUp() throws Exception {
+        Setup setup = new Setup(setupFilepath);
+        outputDir = new File(setup.getOutputDirectory());
+
+        generator = new SimpleParallelGenerator(setup);
+        generator.setVerbose(false);
+    }
+
+
+    /**
+     * Test method for {@link nda.analysis.generation.SimpleParallelGenerator#generate()}.
+     */
+    @Test
+    public void testGenerate() throws Exception {
+        generator.generate();
+        assertTrue(outputDir.listFiles().length > 0);
+    }
+}
