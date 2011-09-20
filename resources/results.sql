b23bfe1f6708da4bdf64e1282b9e3130a0b45d3e
giulianoxt <giulianoxt@ede21ce0-ec6d-f1c9-52f8-815c3ec13f0c>
2011-09-20 14:38:41 +0000

    Add the MySQL db scheme to the repo

---
+++ resources/results.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.3.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Sep 17, 2011 at 07:04 PM
+-- Server version: 5.1.53
+-- PHP Version: 5.3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `neurobjects_new`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `results`
+--
+
+CREATE TABLE IF NOT EXISTS `results` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `animal` varchar(15) NOT NULL,
+  `area` varchar(15) NOT NULL,
+  `object` varchar(15) NOT NULL,
+  `round` int(11) NOT NULL,
+  `model` varchar(30) NOT NULL,
+  `bin_size` double NOT NULL,
+  `window_size` int(11) NOT NULL,
+  `neuron_drop` int(11) DEFAULT NULL,
+  `surrogate` enum('uniform','poisson','col_swap','neuron_swap','matrix_swap') DEFAULT NULL,
+  `num_surrogate` int(11) DEFAULT NULL,
+  `pct_surrogate` double DEFAULT NULL,
+  `num_instances` int(11) NOT NULL,
+  `correct_instances` int(11) NOT NULL,
+  `auroc` double NOT NULL,
+  `kappa` double NOT NULL,
+  `yes_fmeasure` double NOT NULL,
+  `yes_fp` int(11) NOT NULL,
+  `yes_fn` int(11) NOT NULL,
+  `no_fmeasure` double NOT NULL,
+  `no_fp` int(11) NOT NULL,
+  `no_fn` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `CONTEXT` (`animal`,`area`,`object`,`round`,`model`,`bin_size`,`window_size`,`neuron_drop`,`surrogate`,`num_surrogate`,`pct_surrogate`),
+  KEY `MAIN` (`animal`,`area`,`object`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Reference results table.' AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
