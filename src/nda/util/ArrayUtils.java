34a550539a5d2ac782b7a3ee39ebc94b215ac137
giulianoxt <giulianoxt@ede21ce0-ec6d-f1c9-52f8-815c3ec13f0c>
2011-09-20 14:37:16 +0000

    Test if the generated datasets are right

--- src/nda/util/ArrayUtils.java
+++ src/nda/util/ArrayUtils.java
@@ -139,4 +139,16 @@ public final class ArrayUtils {
 
         return true;
     }
+
+
+    public static boolean equals(byte[] a, byte[] b) {
+        if (a.length != b.length)
+            return false;
+
+        for (int i = 0; i < a.length; ++i)
+            if (a[i] != b[i])
+                return false;
+
+        return true;
+    }
 }
