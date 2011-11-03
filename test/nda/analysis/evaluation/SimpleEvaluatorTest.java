b6f52592d82404c4a16921335c811c77655a0bcf
giulianoxt <giulianoxt@ede21ce0-ec6d-f1c9-52f8-815c3ec13f0c>
2011-11-03 18:50:04 +0000

    First unit tests for evaluation components

---
+++ test/nda/analysis/evaluation/SimpleEvaluatorTest.java
@@ -0,0 +1,59 @@
+package nda.analysis.evaluation;
+
+import static org.junit.Assert.assertTrue;
+
+import java.security.MessageDigest;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import weka.classifiers.Evaluation;
+
+import nda.analysis.generation.GeneratorSetup;
+import nda.util.ArrayUtils;
+
+
+/**
+ * Tests for the SimpleEvaluator class.
+ * 
+ * @author Giuliano Vilela
+ */
+public class SimpleEvaluatorTest {
+    private static final String testDirpath = "data/test/train_test_eval";
+    private static final String genSetupFilepath = testDirpath + "/setup.yml";
+    private static final String evalSetupFilepath = testDirpath + "/evaluator.yml";
+
+    private SimpleEvaluator evaluator;
+
+    // Integrity hashes
+    private static final byte[] HASH_TRAIN_TEST = {-29,92,11,29,-21,-86,-120,-3,-85,87,-108,47,37,-12,-53,51};
+
+
+    @Before
+    public void setUp() throws Exception {
+        GeneratorSetup generatorSetup = new GeneratorSetup(genSetupFilepath);
+        EvaluatorSetup evaluatorSetup = new EvaluatorSetup(evalSetupFilepath);
+        evaluator = new SimpleEvaluator(generatorSetup, evaluatorSetup);
+    }
+
+
+    /**
+     * Test method for {@link nda.analysis.evaluation.SimpleEvaluator#evaluate()}.
+     */
+    @Test
+    public void testEvaluate() throws Exception {
+        MessageDigest digest = MessageDigest.getInstance("MD5");
+
+        List<EvaluationResult> results = evaluator.evaluate();
+        for (EvaluationResult result : results) {
+            List<Evaluation> modelEvaluations = result.getModelEvaluations();
+
+            for (Evaluation evaluation : modelEvaluations)
+                digest.update(evaluation.toSummaryString().getBytes("UTF-8"));
+        }
+
+        byte[] hash = digest.digest();
+        assertTrue(ArrayUtils.equals(HASH_TRAIN_TEST, hash));
+    }
+}
